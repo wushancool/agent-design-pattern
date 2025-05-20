681164097b8a4a7d9d78055ad2dbf07b66268e7b
dave wu <dave.wu.mail@gmail.com>
2025-05-20 13:08:48 +0800

    upgrade to python 3.11

--- src/agent_execution/setup.py
+++ src/agent_execution/setup.py
@@ -2,5 +2,5 @@ import sys
 from pathlib import Path
 
 def add_tool_path():
-    tool_path = Path(__file__).resolve().parents[1] / 'tools'
+    tool_path = Path(__file__).resolve().parents[1] / 'utils'
     sys.path.append(str(tool_path))
